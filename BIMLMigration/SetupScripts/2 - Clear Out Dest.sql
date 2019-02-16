5570d6bb52cb1e7d481886b2d6276ad476bdd818
Kevin Feasel <feasel@catallaxyservices.com>
2019-02-16 08:23:46 -0500

    Project updates
    Updating project to use variable-based expressions rather than project parameter for customer ID.

--- BIMLMigration/SetupScripts/2 - Clear Out Dest.sql
+++ BIMLMigration/SetupScripts/2 - Clear Out Dest.sql
@@ -3,5 +3,13 @@ GO
 TRUNCATE TABLE dbo.FactInternetSalesReason
 TRUNCATE TABLE dbo.FactSurveyResponse
 DELETE FROM dbo.FactInternetSales 
-DELETE FROM dbo.DimSalesReason
+DELETE FROM dbo.DimCurrency
 DELETE FROM dbo.DimCustomer
+DELETE FROM dbo.DimDate
+DELETE FROM dbo.DimProduct
+DELETE FROM dbo.DimPromotion
+DELETE FROM dbo.DimSalesReason
+DELETE FROM dbo.DimGeography
+DELETE FROM dbo.DimProductSubcategory
+DELETE FROM dbo.DimProductCategory
+DELETE FROM dbo.DimSalesTerritory
